9c8925421902ba3aa418664661c26e850bfa692b
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2023-06-06 00:36:33 +0300

    Make clock work with useInterval

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,3 +1,4 @@
+import { useState, useCallback, useEffect } from 'react';
 import {
   Button,
   HStack,
@@ -9,30 +10,55 @@ import {
   MenuList,
   Text,
   VStack,
+  useInterval,
 } from '@chakra-ui/react';
 import { BiChevronDown } from 'react-icons/bi';
 import BotQR from './BotQR';
 
-const Footer = () => (
-  <HStack justify="space-between">
-    <VStack>
-      <BotQR />
-      <Text fontFamily="mono">@ITHelpFAQ_bot</Text>
-    </VStack>
-    <VStack>
-      <Heading size="4xl">12:34</Heading>
-      <Text fontSize="xl">Friday, 12 May</Text>
-    </VStack>
-    <Menu>
-      <MenuButton as={Button} variant="outline" rightIcon={<Icon boxSize="5" as={BiChevronDown} />}>
-        Language
-      </MenuButton>
-      <MenuList>
-        <MenuItem>English</MenuItem>
-        <MenuItem>Russian</MenuItem>
-      </MenuList>
-    </Menu>
-  </HStack>
-);
+const Footer = () => {
+  const [date, setDate] = useState<Date>();
+
+  const updateDate = useCallback(() => {
+    const newDate = new Date();
+    if (newDate.getMinutes() !== date?.getMinutes()) {
+      setDate(newDate);
+    }
+  }, [date]);
+
+  useEffect(updateDate, [updateDate]);
+  useInterval(updateDate, 1000);
+
+  return (
+    <HStack justify="space-between">
+      <VStack>
+        <BotQR />
+        <Text fontFamily="mono">@ITHelpFAQ_bot</Text>
+      </VStack>
+      {date && (
+        <VStack>
+          <Heading size={['xl', null, null, '2xl', '4xl']}>
+            {date.toLocaleString('en', { timeStyle: 'short' })}
+          </Heading>
+          <Text fontSize={['md', null, null, 'xl']}>
+            {date.toLocaleString('en', { dateStyle: 'full' })}
+          </Text>
+        </VStack>
+      )}
+      <Menu>
+        <MenuButton
+          as={Button}
+          variant="outline"
+          rightIcon={<Icon boxSize="5" as={BiChevronDown} />}
+        >
+          Language
+        </MenuButton>
+        <MenuList>
+          <MenuItem>English</MenuItem>
+          <MenuItem>Russian</MenuItem>
+        </MenuList>
+      </Menu>
+    </HStack>
+  );
+};
 
 export default Footer;
