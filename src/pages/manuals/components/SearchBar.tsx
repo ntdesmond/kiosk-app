9e07de17246c25dbfdbe857c009934f5f8398b3d
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2023-06-14 20:54:21 +0300

    Make SearchBar hide keyboard on outside click

--- src/pages/manuals/components/SearchBar.tsx
+++ src/pages/manuals/components/SearchBar.tsx
@@ -1,5 +1,13 @@
-import { Collapse, Input, InputGroup, InputLeftElement, useDisclosure } from '@chakra-ui/react';
+import {
+  Box,
+  Collapse,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { useRef } from 'react';
+import { useOnClickOutside } from 'usehooks-ts';
 import { useTranslation } from 'react-i18next';
 import { MdSearch } from 'react-icons/md';
 import Keyboard from '../../../components/forms/keyboard/Keyboard';
@@ -7,20 +15,25 @@ import Keyboard from '../../../components/forms/keyboard/Keyboard';
 const SearchBar = ({ onChange }: { onChange: (value: string) => void }) => {
   const { t } = useTranslation();
   const { isOpen, onClose, onOpen } = useDisclosure();
-  const ref = useRef<HTMLInputElement>(null);
+  const ref = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useOnClickOutside(ref, onClose);
 
   return (
-    <>
+    <Box ref={ref}>
       <InputGroup marginBottom="8">
         <InputLeftElement>
           <MdSearch />
         </InputLeftElement>
-        <Input placeholder={t('search') as string} ref={ref} onClick={onOpen} />
+        <Input placeholder={t('search') as string} ref={inputRef} onClick={onOpen} />
       </InputGroup>
       <Collapse in={isOpen}>
-        <Keyboard inputRef={ref} onDone={onClose} onInputChange={onChange} />
+        <Box paddingBottom="8">
+          <Keyboard inputRef={inputRef} onDone={onClose} onInputChange={onChange} />
+        </Box>
       </Collapse>
-    </>
+    </Box>
   );
 };
 
