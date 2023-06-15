70a49669bf556a7c157b028adeb60e66a484d1e8
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2023-06-15 16:47:40 +0300

    Add --debug cli parameter

--- electron/main.ts
+++ electron/main.ts
@@ -9,7 +9,7 @@ const dist_path = path.join(__dirname, '../dist');
 const { VITE_DEV_SERVER_URL } = process.env;
 
 const argv = minimist(process.argv.slice(process.argv.indexOf('--') + 1), {
-  default: { width: 1920, height: 1080, kiosk: true, relaunch: false, update: true },
+  default: { width: 1920, height: 1080, kiosk: true, relaunch: false, update: true, debug: false },
   alias: { width: 'w', height: 'h' },
 });
 
@@ -21,12 +21,12 @@ function createWindow() {
     height: argv.height as number,
     autoHideMenuBar: true,
     webPreferences: {
-      devTools: import.meta.env.DEV,
+      devTools: import.meta.env.DEV || argv.debug as boolean,
       preload: path.join(__dirname, 'preload.js'),
     },
   });
 
-  if (import.meta.env.PROD) {
+  if (import.meta.env.PROD && !(argv.debug as boolean)) {
     window.removeMenu();
   }
 
