5c9ee62d1df4707baa4700935e408c9bf824b69a
Vladislav Safonov <29694249+ntdesmond@users.noreply.github.com>
2023-07-26 00:06:54 +0300

    Disable automatic updates as they are broken

--- electron/main.ts
+++ electron/main.ts
@@ -9,7 +9,7 @@ const dist_path = path.join(__dirname, '../dist');
 const { VITE_DEV_SERVER_URL } = process.env;
 
 const argv = minimist(process.argv.slice(process.argv.indexOf('--') + 1), {
-  default: { width: 1920, height: 1080, kiosk: true, relaunch: false, update: true, debug: false },
+  default: { width: 1920, height: 1080, kiosk: true, relaunch: false, update: false, debug: false },
   alias: { width: 'w', height: 'h' },
 });
 
